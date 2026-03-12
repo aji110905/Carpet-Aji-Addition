f4a15eee0784ede6cc8c5d6a62070dce8ab15116
aji <2442733838@qq.com>
2026-03-13 01:31:37 +0800

    refactor(follow): 优化物品跟随功能的计分板操作逻辑

--- src/main/java/aji/carpetajiaddition/mixin/command/follow/ItemEntityMixin.java
+++ src/main/java/aji/carpetajiaddition/mixin/command/follow/ItemEntityMixin.java
@@ -2,12 +2,14 @@ package aji.carpetajiaddition.mixin.command.follow;
 
 import aji.carpetajiaddition.CarpetAjiAdditionSettings;
 import aji.carpetajiaddition.commands.FollowCommand;
+import net.minecraft.server.ServerScoreboard;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.TraceableEntity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.scores.PlayerTeam;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -25,13 +27,18 @@ public abstract class ItemEntityMixin extends Entity implements TraceableEntity 
 
     @Inject(method = "tick", at = @At("HEAD"))
     private void tick(CallbackInfo ci) {
+        ServerScoreboard scoreboard = CarpetAjiAdditionSettings.minecraftServer.getScoreboard();
+        PlayerTeam team = scoreboard.getPlayerTeam("followItems");
+        if (team == null) {
+            return;
+        }
         if (FollowCommand.data.getFollowItems().contains(getItem().getItem())) {
-            CarpetAjiAdditionSettings.minecraftServer.getScoreboard().addPlayerToTeam(getUUID().toString(), CarpetAjiAdditionSettings.minecraftServer.getScoreboard().getPlayerTeam("followItems"));
+            scoreboard.addPlayerToTeam(getUUID().toString(), team);
             setGlowingTag(true);
         } else {
-            CarpetAjiAdditionSettings.minecraftServer.getScoreboard().getPlayerTeam("followItems").getPlayers().forEach(uuidString -> {
+            team.getPlayers().forEach(uuidString -> {
                 if (uuidString.equals(getUUID().toString())) {
-                    CarpetAjiAdditionSettings.minecraftServer.getScoreboard().removePlayerFromTeam(uuidString, CarpetAjiAdditionSettings.minecraftServer.getScoreboard().getPlayerTeam("followItems"));
+                    scoreboard.removePlayerFromTeam(uuidString, team);
                 }
             });
             setGlowingTag(false);
