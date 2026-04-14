4aea81e366ac98065e5ca1bf2f1e544b501f30f3
aji <2442733838@qq.com>
2026-04-14 16:53:12 +0800

    合并CustomRecipe到RecipManager

--- src/main/java/aji/carpetajiaddition/recipe/RecipeManager.java
+++ src/main/java/aji/carpetajiaddition/recipe/RecipeManager.java
@@ -1,13 +1,32 @@
 package aji.carpetajiaddition.recipe;
 
 import aji.carpetajiaddition.CarpetAjiAdditionSettings;
+import aji.carpetajiaddition.recipe.builder.ShapedRecipeBuilder;
 import aji.carpetajiaddition.recipe.template.ShapedRecipeTemplate;
+import aji.carpetajiaddition.settings.RuleCategory;
+import aji.carpetajiaddition.util.MinecraftServerUtil;
+import carpet.api.settings.Rule;
 import com.google.gson.JsonElement;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.crafting.RecipeHolder;
+//#if MC > 12102
+//$$ import net.minecraft.core.HolderLookup;
+//$$ import net.minecraft.world.item.crafting.Recipe;
+//#endif
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+
+import static net.minecraft.world.item.Items.*;
+import static net.minecraft.world.item.Items.DRAGON_BREATH;
+import static net.minecraft.world.item.Items.DRAGON_EGG;
+import static net.minecraft.world.item.Items.EGG;
+import static net.minecraft.world.item.Items.END_CRYSTAL;
+import static net.minecraft.world.item.Items.GLASS_BOTTLE;
 
 public class RecipeManager {
     private static final List<ShapedRecipeTemplate> shapedRecipeList = new ArrayList<>();
@@ -16,11 +35,91 @@ public class RecipeManager {
         shapedRecipeList.add(new ShapedRecipeTemplate(ResourceLocation.fromNamespaceAndPath(CarpetAjiAdditionSettings.MOD_ID, id), pattern, ingredients, result, count));
     }
 
+    //#if MC < 12102
     public static void registerRecipes(Map<ResourceLocation, JsonElement> recipeMap) {
         shapedRecipeList.forEach(recipe -> recipe.addToRecipeMap(recipeMap));
+    //#else
+    //$$ public static void registerRecipes(Map<ResourceLocation, Recipe<?>> recipeMap, HolderLookup.Provider provider) {
+    //$$     shapedRecipeList.forEach(recipe -> recipe.addToRecipeMap(recipeMap, provider));
+    //#endif
     }
 
     public static void clearRecipeListMemory() {
         shapedRecipeList.clear();
+    }
+
+    public static void buildRecipes(){
+        ShapedRecipeBuilder.create(CarpetAjiAdditionSettings.dragonEggRecipe, "dragon_egg")
+                .pattern("&#&")
+                .pattern("^*^")
+                .pattern("$$$")
+                .define('&', CRYING_OBSIDIAN).define('#', GLASS_BOTTLE).define('^', OBSIDIAN).define('*', EGG).define('$', END_CRYSTAL)
+                .output(DRAGON_EGG, 1).build();
+
+        ShapedRecipeBuilder.create(CarpetAjiAdditionSettings.dragonBreathRecipe, "dragon_breath")
+                .pattern("#")
+                .pattern("*")
+                .define('#', DRAGON_EGG).define('*', GLASS_BOTTLE)
+                .output(DRAGON_BREATH, 1).build();
+    }
+
+    public static void onRuleValueChanged(){
+        if (MinecraftServerUtil.serverIsRunning()) {
+            RecipeManager.clearRecipeListMemory();
+            buildRecipes();
+            CarpetAjiAdditionSettings.minecraftServer.execute(() -> {
+                needReloadServerResources();
+                for (RecipeHolder<?> recipe : CarpetAjiAdditionSettings.minecraftServer.getRecipeManager().getRecipes()) {
+                    //#if MC < 12102
+                    if (recipe.id().getNamespace().equals(CarpetAjiAdditionSettings.MOD_ID)) {
+                        //#else
+                        //$$ if (recipe.id().location().getNamespace().equals(CarpetAjiAdditionSettings.MOD_ID)) {
+                        //#endif
+                        for (ServerPlayer player : CarpetAjiAdditionSettings.minecraftServer.getPlayerList().getPlayers()) {
+                            if (!player.getRecipeBook().contains(recipe.id())) {
+                                player.awardRecipes(List.of(recipe));
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    public static void onPlayerLoggedIn(ServerPlayer player){
+        if (MinecraftServerUtil.serverIsRunning() && hasActiveRecipeRule()) {
+            for (RecipeHolder<?> recipe : CarpetAjiAdditionSettings.minecraftServer.getRecipeManager().getRecipes()) {
+                //#if MC < 12102
+                if (recipe.id().getNamespace().equals(CarpetAjiAdditionSettings.MOD_ID) && !player.getRecipeBook().contains(recipe.id())) {
+                    //#else
+                    //$$ if (recipe.id().location().getNamespace().equals(CarpetAjiAdditionSettings.MOD_ID) && !player.getRecipeBook().contains(recipe.id())) {
+                    //#endif
+                    player.awardRecipes(List.of(recipe));
+                }
+            }
+        }
+    }
+
+    private static boolean hasActiveRecipeRule() {
+        Field[] fields = CarpetAjiAdditionSettings.class.getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(Rule.class)) {
+                try {
+                    field.setAccessible(true);
+                    if (Arrays.asList(field.getAnnotation(Rule.class).categories()).contains(RuleCategory.RECIPE) && field.getBoolean(null)) {
+                        return true;
+                    }
+                } catch (IllegalAccessException e) {
+                    CarpetAjiAdditionSettings.LOGGER.error("Failed to get rule value", e);
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void needReloadServerResources() {
+        if (MinecraftServerUtil.serverIsRunning() && hasActiveRecipeRule()) {
+            CarpetAjiAdditionSettings.minecraftServer.reloadResources(CarpetAjiAdditionSettings.minecraftServer.getPackRepository().getSelectedIds());
+        }
     }
 }
--- versions/1.21.2/src/main/java/aji/carpetajiaddition/mixin/rules/recipeRule/RecipeManagerMixin.java
+++ versions/1.21.2/src/main/java/aji/carpetajiaddition/mixin/rules/recipeRule/RecipeManagerMixin.java
@@ -1,6 +1,5 @@
 package aji.carpetajiaddition.mixin.rules.recipeRule;
 
-import aji.carpetajiaddition.recipe.CustomRecipes;
 import aji.carpetajiaddition.recipe.RecipeManager;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.resources.ResourceLocation;
@@ -24,7 +23,7 @@ public abstract class RecipeManagerMixin {
     )
     private void addCustomRecipes(CallbackInfoReturnable<RecipeMap> cir, @Local(name = "sortedMap") SortedMap<ResourceLocation, Recipe<?>> recipes) {
         RecipeManager.clearRecipeListMemory();
-        CustomRecipes.buildRecipes();
+        RecipeManager.buildRecipes();
         RecipeManager.registerRecipes(recipes, ((RecipeManagerAccessor) this).getRegistries());
     }
 }
