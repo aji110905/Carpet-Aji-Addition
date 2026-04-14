124818517a96258b54a68fe6bdf12269937ad14d
aji <2442733838@qq.com>
2026-04-14 16:53:12 +0800

    修复版本条件判断逻辑

--- src/main/java/aji/carpetajiaddition/mixin/rules/recipeRule/RecipeManagerMixin.java
+++ src/main/java/aji/carpetajiaddition/mixin/rules/recipeRule/RecipeManagerMixin.java
@@ -1,6 +1,5 @@
 package aji.carpetajiaddition.mixin.rules.recipeRule;
 
-import aji.carpetajiaddition.recipe.CustomRecipes;
 import aji.carpetajiaddition.recipe.RecipeManager;
 import com.google.gson.JsonElement;
 import net.minecraft.resources.ResourceLocation;
@@ -15,7 +14,7 @@ public abstract class RecipeManagerMixin {
     @ModifyVariable(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At("HEAD"), argsOnly = true)
     private Map<ResourceLocation, JsonElement> registerCustomRecipes(Map<ResourceLocation, JsonElement> map) {
         RecipeManager.clearRecipeListMemory();
-        CustomRecipes.buildRecipes();
+        RecipeManager.buildRecipes();
         RecipeManager.registerRecipes(map);
         return map;
     }
