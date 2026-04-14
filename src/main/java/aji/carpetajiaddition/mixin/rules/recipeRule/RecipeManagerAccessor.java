69a1e4b5bc4114ddd82f09f2e952a09b383b0ea1
aji <2442733838@qq.com>
2026-04-14 17:53:43 +0800

    改个包名

--- src/main/java/aji/carpetajiaddition/mixin/rules/recipeRule/RecipeManagerAccessor.java
+++ src/main/java/aji/carpetajiaddition/mixin/rules/recipeRule/RecipeManagerAccessor.java
@@ -1,6 +1,6 @@
 package aji.carpetajiaddition.mixin.rules.recipeRule;
 
-import aji.carpetajiaddition.util.Compat.DummyClass;
+import aji.carpetajiaddition.util.compatible.DummyClass;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(DummyClass.class)
