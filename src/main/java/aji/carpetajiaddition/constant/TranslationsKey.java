5c69bd96ccbcab6fb375ef6e0d4e68454091d70d
aji <2442733838@qq.com>
2026-05-27 14:13:26 +0800

    移除未使用的验证器翻译键

--- src/main/java/aji/carpetajiaddition/constant/TranslationsKey.java
+++ src/main/java/aji/carpetajiaddition/constant/TranslationsKey.java
@@ -6,6 +6,4 @@ public class TranslationsKey {
     public static final String COMMAND = SUFFIX + "command.";
     public static final String CMD_FOLLOW = COMMAND + "follow.";
     public static final String CMD_MODS = COMMAND + "mods.";
-
-    public static final String VALIDATOR = SUFFIX + "validator.";
 }
